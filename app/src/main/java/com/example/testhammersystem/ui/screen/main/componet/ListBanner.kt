856fb918d7acc3d41d8bcd837522adcdd41e9acc
nurik <hhhhghhghu@gmail.com>
2022-10-13 20:24:15 +0300

    added top app bar

---
+++ app/src/main/java/com/example/testhammersystem/ui/screen/main/componet/ListBanner.kt
@@ -0,0 +1,65 @@
+package com.example.testhammersystem.ui.screen.main.componet
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.testhammersystem.util.lerp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import kotlin.math.absoluteValue
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HorizontalPagerWithOffsetTransition(modifier: Modifier = Modifier) {
+    HorizontalPager(
+        count = 10,
+        contentPadding = PaddingValues(start = 16.dp, end = 42.dp),
+        modifier = modifier.fillMaxWidth()
+    ) { page ->
+        Card(
+            Modifier
+                .graphicsLayer {
+                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                    // We animate the scale, between 50% and 100%
+                    lerp(
+                        start = 0.90f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    ).also { scale ->
+                        scaleX = scale
+                        scaleY = scale
+                    }
+
+                    // We animate the alpha, between 50% and 100%
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+                .fillMaxWidth()
+                .aspectRatio(2.70f),
+            shape = MaterialTheme.shapes.medium
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize(),
+                model = "https://get.wallhere.com/photo/food-pizza-tomatoes-1719701.jpg",
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
