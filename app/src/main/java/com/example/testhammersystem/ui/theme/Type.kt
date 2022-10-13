37c0bf42d2b0428e87ee710b11c2b2b5a4a733fb
nurik <hhhhghhghu@gmail.com>
2022-10-13 15:31:39 +0300

    added typography

--- app/src/main/java/com/example/testhammersystem/ui/theme/Type.kt
+++ app/src/main/java/com/example/testhammersystem/ui/theme/Type.kt
@@ -1,28 +1,52 @@
 package com.example.testhammersystem.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.testhammersystem.R
 
-// Set of Material typography styles to start with
+private val Roboto = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.SemiBold),
+)
+private val SfUiDisplay = FontFamily(
+    Font(R.font.sf_ui_display, FontWeight.Normal),
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    subtitle1 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp,
+        lineHeight = 18.75.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
+
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = SfUiDisplay,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 14.sp,
+        lineHeight = 16.71.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
+
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = SfUiDisplay,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 13.sp,
+        lineHeight = 15.51.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontFamily = SfUiDisplay,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
+    ),
 )
